d41764ff14747948aa15ae624174f7f0d484c238
Rohit Yadav <me.rohit.ydv@gmail.com>
2023-03-09 20:15:50 +0530

    feat: get name of all contents

--- src/controllers/content.js
+++ src/controllers/content.js
@@ -23,30 +23,30 @@ async function getAllContents(req, res){
     }
 }
 
-async function addFieldToContent(req, res){
-    try{
-        const result = await services.addFieldToContent(req.body);
-        if(result==="content with given name doesn't exist" || result==="content already has specified field")
-            return res.status(400).send(result);
-        return res.status(200).send(result);
-    }
-    catch(e){
-        console.log(e);
-        return res.status(500).send("Something went wrong.");
-    }
-}
+// async function addFieldToContent(req, res){
+//     try{
+//         const result = await services.addFieldToContent(req.body);
+//         if(result==="content with given name doesn't exist" || result==="content already has specified field")
+//             return res.status(400).send(result);
+//         return res.status(200).send(result);
+//     }
+//     catch(e){
+//         console.log(e);
+//         return res.status(500).send("Something went wrong.");
+//     }
+// }
 
-async function deleteFieldFromContent(req, res){
-    try{
-        const result = await services.deleteFieldFromContent(req.body);
-        if(result==="content with given name doesn't exist")
-            return res.status(400).send(result);
-        return res.status(200).send(result);
-    }
-    catch(e){
-        console.log(e);
-        return res.status(500).send("Something went wrong.");
-    }
-}
+// async function deleteFieldFromContent(req, res){
+//     try{
+//         const result = await services.deleteFieldFromContent(req.body);
+//         if(result==="content with given name doesn't exist")
+//             return res.status(400).send(result);
+//         return res.status(200).send(result);
+//     }
+//     catch(e){
+//         console.log(e);
+//         return res.status(500).send("Something went wrong.");
+//     }
+// }
 
 module.exports = {createContent, getAllContents, addFieldToContent, deleteFieldFromContent};
--- src/services/content.js
+++ src/services/content.js
@@ -23,42 +23,42 @@ async function getAllContents(){
     return await ContentsTable.findAll( { attributes:['contentName'] } );
 }
 
-async function addFieldToContent(reqBody){
-    const {contentName, fieldName} = reqBody;
-    const content = await findContent(contentName);
-    if(content===null)
-        return "content with given name doesn't exist";
+// async function addFieldToContent(reqBody){
+//     const {contentName, fieldName} = reqBody;
+//     const content = await findContent(contentName);
+//     if(content===null)
+//         return "content with given name doesn't exist";
     
-    let contentFields = content.dataValues.contentFields;
+//     let contentFields = content.dataValues.contentFields;
     
-    const updatedFields = [];
-    for (const [key, value] of Object.entries(contentFields)) {
-        updatedFields.push(value);
-    }
-    updatedFields.push(fieldName);
+//     const updatedFields = [];
+//     for (const [key, value] of Object.entries(contentFields)) {
+//         updatedFields.push(value);
+//     }
+//     updatedFields.push(fieldName);
 
-    if(contentFields.includes(fieldName))
-        return "content already has specified field";
+//     if(contentFields.includes(fieldName))
+//         return "content already has specified field";
     
-    await ContentsTable.update({contentFields: updatedFields}, {where: {contentName: contentName}});
-    return {contentName};
-}
+//     await ContentsTable.update({contentFields: updatedFields}, {where: {contentName: contentName}});
+//     return {contentName};
+// }
 
-async function deleteFieldFromContent(reqBody){
-    const {contentName, fieldName} = reqBody;
-    const content = await findContent(contentName);
-    if(content===null)
-        return "content with given name doesn't exist";
+// async function deleteFieldFromContent(reqBody){
+//     const {contentName, fieldName} = reqBody;
+//     const content = await findContent(contentName);
+//     if(content===null)
+//         return "content with given name doesn't exist";
     
-    let contentFields = content.dataValues.contentFields;
-    const updatedFields = [];
-    for (const [key, value] of Object.entries(contentFields)) {
-        if(value!==fieldName)
-        updatedFields.push(value);
-    }
+//     let contentFields = content.dataValues.contentFields;
+//     const updatedFields = [];
+//     for (const [key, value] of Object.entries(contentFields)) {
+//         if(value!==fieldName)
+//         updatedFields.push(value);
+//     }
     
-    await ContentsTable.update({contentFields: updatedFields}, {where: {contentName: contentName}});
-    return {contentName};
-}
+//     await ContentsTable.update({contentFields: updatedFields}, {where: {contentName: contentName}});
+//     return {contentName};
+// }
 
 module.exports = { createContent, getAllContents, addFieldToContent, deleteFieldFromContent };
